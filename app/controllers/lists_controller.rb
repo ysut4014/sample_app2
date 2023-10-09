968d1732ac8770ebfb5ca51edbf8d7fb545bd375
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2023-10-09 14:24:24 +0000

    5章【編集機能を作ろう】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,12 +8,25 @@ class ListsController < ApplicationController
     redirect_to '/top'
   end  
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
   private
   #ストロング
